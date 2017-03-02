489d1244c3fba683b1be06731ee1ac54674441af
Emile Fortin <sylfschiffer@gmail.com>
2017-03-02 15:22:34 -0500

    création du .designer

--- TicTacToe_Client/TicTacToe_Client/MainGame.cpp
+++ TicTacToe_Client/TicTacToe_Client/MainGame.cpp
@@ -1,15 +1,15 @@
-#include "Connect.h"
+#include "MainGame.h"
 
 using namespace System;
 using namespace System::Windows::Forms;
 
 
 [STAThread]
-void main2()
+void main()
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 
-	TicTacToe_Client::Connect form;
+	TicTacToe_Client::MainGame form;
 	Application::Run(%form);
 }
